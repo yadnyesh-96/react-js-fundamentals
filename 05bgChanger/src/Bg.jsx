e0f38a35a9812627fb6e4420528935bb35c23243
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-08 13:59:12 +0530

    bg changer in react using hooks

--- 05bgChanger/src/Bg.jsx
+++ 05bgChanger/src/Bg.jsx
@@ -2,10 +2,43 @@
 
 import './index.css'
 
-function changer(){
+function changer() {
 
     return (
-        <h2>Bg Chnager</h2>
+        <div class="w-full h-screen flex items-center justify-center bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900">
+
+
+            <div class="absolute bottom-10 w-full flex justify-center">
+
+                <div class="flex gap-4 backdrop-blur-md bg-white/10 border border-white/20 shadow-2xl px-6 py-4 rounded-3xl">
+
+
+                    <button
+                        class="px-5 py-2 rounded-full text-white font-semibold bg-red-500 hover:bg-red-600 hover:scale-110 transition duration-300 shadow-lg">
+                        Red
+                    </button>
+
+
+                    <button
+                        class="px-5 py-2 rounded-full text-white font-semibold bg-green-500 hover:bg-green-600 hover:scale-110 transition duration-300 shadow-lg">
+                        Green
+                    </button>
+
+                    <button
+                        class="px-5 py-2 rounded-full text-white font-semibold bg-blue-500 hover:bg-blue-600 hover:scale-110 transition duration-300 shadow-lg">
+                        Blue
+                    </button>
+
+                    <button
+                        class="px-5 py-2 rounded-full text-white font-semibold bg-purple-500 hover:bg-purple-600 hover:scale-110 transition duration-300 shadow-lg">
+                        Purple
+                    </button>
+
+                </div>
+
+            </div>
+
+        </div>
     )
 }
 
