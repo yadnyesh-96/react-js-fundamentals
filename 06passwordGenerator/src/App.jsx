d6a29f99167d3d7ef8404cc69a5ccfdf171b9476
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-11 17:13:16 +0530

    passwors Generator project

--- 06passwordGenerator/src/App.jsx
+++ 06passwordGenerator/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
     <>
       <body className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
         
-       
       </body>
 
     </>
