ca355c86f3ecaa01cd2d5a6b3b255f9063559110
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-03-25 00:07:58 +0530

    passWord Generator Application

--- 07hooks/src/App.js
+++ 07hooks/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useState } from "react";
 function App() {
+  let msg;
+  const [count, setCount] = useState(0)
+  if(count>10){
+    msg="High"
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={() => { setCount(count + 1) }}>+</button>
+      <button onClick={() => { setCount(count - 1) }}>-</button>
+      <h1>{msg}</h1>
     </div>
   );
 }
