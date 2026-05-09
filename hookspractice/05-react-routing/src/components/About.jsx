f7e559de0bfa5006254e7d766797c09a93c9bee8
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-05-09 14:32:54 +0530

    react-routing

--- hookspractice/05-react-routing/src/components/About.jsx
+++ hookspractice/05-react-routing/src/components/About.jsx
@@ -5,7 +5,7 @@ function About() {
         <div className="flex flex-col p-40 items-center justify-center">
             <h1>About Page</h1>
         </div>
-    )  
+    )   
 }
 
 export default About;
