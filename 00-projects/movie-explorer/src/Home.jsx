9ea33a05cdf0e1c171728b5dfbe6d5a49a5e64ee
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-06-16 18:13:42 +0530

    Basic Routing has done

--- 00-projects/movie-explorer/src/Home.jsx
+++ 00-projects/movie-explorer/src/Home.jsx
@@ -1,11 +1,10 @@
 import { useEffect, useState } from "react";
 import { searchMovies } from "../service/movieApi";
-
-import Movies from "./Movies.jsx";
-
+import MovieCard from "../components/MovieCard";
 
 function Home() {
 
+    const [search, setSearch] = useState("batman");
     const [movies, setMovies] = useState([]);
 
     useEffect(() => {
@@ -13,18 +12,34 @@ function Home() {
     }, [])
 
     async function loadMovies() {
-        const data = await searchMovies("batman");
+        const data = await searchMovies(search);
         setMovies(data);
     }
 
     return (
         <div className="p-6">
-            <h1 className="text-3xl font-bold mb-6">
-                Movie Explorer
-            </h1>
+            <div className="w-full my-5">
+                <h1 className="text-3xl font-bold mb-6">Movie Explorer</h1>
+                <input
+                    type="text"
+                    name=""
+                    id=""
+                    value={search}
+                    onChange={(e) => setSearch(e.target.value)}
+                    className="border p-2 rounded-sm text-sm font-medium"
+                    placeholder="Serach Movies Here"
+                />
+
+                <button
+                    onClick={loadMovies}
+                    className="border mx-2 p-1 px-2 rounded-sm font-bold bg-amber-200"
+                >
+                    Search
+                </button>
+            </div>
             <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
                 {movies.map((movie) => (
-                    <Movies
+                    <MovieCard
                         key={movie.imdbID}
                         movie={movie}
                     />))
