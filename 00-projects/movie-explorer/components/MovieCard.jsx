8c0793b30237669db34fd29ca2718e6b107b1a72
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-06-16 17:52:40 +0530

    Basic Routing has done

--- 00-projects/movie-explorer/components/MovieCard.jsx
+++ 00-projects/movie-explorer/components/MovieCard.jsx
@@ -1,6 +1,15 @@
 
 function MovieCard({ movie }) {
-    
+    return (
+        <div className="border rounded-lg p-6 shadow">
+            <img
+                src={movie.Poster}
+                alt={movie.Title}
+            />
+            <h2 className="font-bold mt-2">{movie.Title}</h2>
+            <p className="font-bold">{movie.year}</p>
+        </div>
+    )
 }
 
 export default MovieCard;
--- 00-projects/movie-explorer/src/Movies.jsx
+++ 00-projects/movie-explorer/src/Movies.jsx
@@ -1,19 +1,7 @@
 
 function Movies({movie}) {
     return (
-        <div className="border rounded-lg p-4 shadow">
-            <img
-                src={movie.Poster}
-                alt={movie.Title}
-                className="w-full h-72 object-cover"
-            />
-
-            <h2 className="font-bold mt-2">
-                {movie.Title}
-            </h2>
-
-            <p>{movie.Year}</p>
-        </div>
+        <h1>Movie Page</h1>
     )
 }
 
