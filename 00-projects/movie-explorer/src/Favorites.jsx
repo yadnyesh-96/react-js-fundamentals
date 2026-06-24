5928a92a2ca6275422030b9a4a63aa9c1e84cbdb
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-06-24 23:01:54 +0530

    favorite movie button

--- 00-projects/movie-explorer/src/Favorites.jsx
+++ 00-projects/movie-explorer/src/Favorites.jsx
@@ -12,16 +12,29 @@ function Favorite() {
 
         setFavorites(savedFavorites);
     }, []);
-    
+
+    function handleRemoveMov(movieId) {
+        const updatedFavorites = favorites.filter(
+            movie => movie.imdbID !== movieId
+        );
+
+        setFavorites(updatedFavorites);
+
+        localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
+    }
     return (
         <>
             <div className="p-6">
                 <h2>Favorites({favorites.length})</h2>
                 <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4">
                     {favorites.map((movie) => (
-                        <MovieCard key={movie.imdbID}
-                            movie={movie}
-                        />
+                        <div key={movie.imdbID}>
+                            <MovieCard movie={movie} />
+
+                            <button onClick={() => handleRemoveMov(movie.imdbID)}>
+                                Remove
+                            </button>
+                        </div>
                     ))}
                 </div>
             </div>
