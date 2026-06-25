72f004ea3cd98140eea83397f44444e90c0959c8
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-06-26 01:22:11 +0530

    favorite movie button

--- 00-projects/movie-explorer/components/MovieCard.jsx
+++ 00-projects/movie-explorer/components/MovieCard.jsx
@@ -1,7 +1,7 @@
 import { Link } from "react-router-dom";
 
 
-function MovieCard({ movie }) {
+function MovieCard({ movie, showFavorite = true }) {
 
     function handleFavorite(e) {
         e.preventDefault();
