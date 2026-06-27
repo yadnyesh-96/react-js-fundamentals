191475708a74083dc724349b219697a967059eca
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-06-27 21:59:27 +0530

    props with childern

--- 00-projects/movie-explorer/src/Movies.jsx
+++ 00-projects/movie-explorer/src/Movies.jsx
@@ -2,22 +2,34 @@ import Card from "./Card";
 
 function Movies() {
     return (
+        <>
+            <Card>
+                <div className="flex flex-col items-center">
+                    <h1 className="text-4xl font-medium border-b-2 pb-1">User Profile</h1>
 
-        <Card>
-            <div className="flex flex-col items-center">
-                <h1 className="text-4xl font-medium border-b-2 pb-1">User Profile</h1>
+                    <div className="flex items-center mt-5">
+                        <h1 className="font-bold text-2xl">Name :</h1>
+                        <h2 className="text-2xl mx-2">Yadnyesh</h2>
+                    </div>
 
-                <div className="flex items-center mt-5">
-                    <h1 className="font-bold text-2xl">Name :</h1>
-                    <h2 className="text-2xl mx-2">Yadnyesh</h2>
+                    <div className="flex items-center mt-5">
+                        <h1 className="font-bold text-2xl">City :</h1>
+                        <h2 className="text-2xl mx-2">Pune</h2>
+                    </div>
                 </div>
+            </Card>
 
-                <div className="flex items-center mt-5">
-                    <h1 className="font-bold text-2xl">City :</h1>
-                    <h2 className="text-2xl mx-2">Pune</h2>
+            <Card>
+                <div className="flex flex-col items-center">
+                    <h2>React Learning</h2>
+                    <ul>
+                        <li>Props</li>
+                        <li>Children</li>
+                        <li>Hooks</li>
+                    </ul>
                 </div>
-            </div>
-        </Card>
+            </Card>
+        </>
     )
 }
 
