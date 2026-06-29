e31166da431ff486efcfaa9d233a0f4e64f40bdd
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-06-29 20:12:05 +0530

    Custom Hooks

--- 00-projects/movie-explorer/src/hooks/useMovie.js
+++ 00-projects/movie-explorer/src/hooks/useMovie.js
@@ -6,6 +6,31 @@ function useMovie() {
     const [Loading, setLoading] = useState(false);
     const [error, setError] = useState("");
 
+    useEffect(() => {
+        loadMovie();
+    }, [id]);
+
+    async function loadMovie() {
+
+        try {
+            setLoading(true);
+            setError("");
+            const data = await serachMovieDetails(id);
+
+            if (data.Response === "False") {
+                setError(data.Error);
+                return;
+            }
+
+            setMovie(data);
+        } catch (err) {
+            setError("Failed to load movie details");
+        } finally {
+            setLoading(false);
+        }
+
+    }
+
     return {
         movie,
         Loading,
