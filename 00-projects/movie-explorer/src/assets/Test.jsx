6cc593d5a50bd3611697a7686c64870ec846e1d4
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-07-11 00:02:32 +0530

    custom hooks

---
+++ 00-projects/movie-explorer/src/assets/Test.jsx
@@ -0,0 +1,21 @@
+
+function Test({ show }) {
+
+    if (show) {
+        return (
+            <div>
+                <h1>Hello React</h1>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1>Hello</h1>
+            </div>
+        )
+    }
+
+
+}
+
+export default Test;
