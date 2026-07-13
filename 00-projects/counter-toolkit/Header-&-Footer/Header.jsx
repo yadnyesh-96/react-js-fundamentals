62de0947fdec969fc9714c19ea0e4d41d32f0833
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-07-14 00:24:26 +0530

    custom hooks and rounting

--- 00-projects/counter-toolkit/Header-&-Footer/Header.jsx
+++ 00-projects/counter-toolkit/Header-&-Footer/Header.jsx
@@ -4,7 +4,7 @@ function Header() {
     return (
         <header className="fixed top-0 left-0 w-full bg-white shadow z-50">
             <div className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
-                <h1 className="text-2xl font-bold">Counter-ToolKit</h1>
+                <h1 className="text-2xl font-bold">Counter - ToolKit</h1>
 
                 <nav>
                     <ul className="flex gap-8">
